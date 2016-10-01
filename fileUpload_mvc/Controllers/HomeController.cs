8b5d03cd2baa591beef949f04721e903312c2560
ismail fedakar <fedakaraydin09@hotmail.com>
2016-10-01 17:29:39 +0300

    upload_Mvc_1
    http://www.ismailfedakar.com

---
+++ fileUpload_mvc/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Objects.DataClasses;
+using System.IO;
+
+namespace fileUpload_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ResimEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ResimEkle(HttpPostedFileBase resimDosyasi)
+        {
+         
+                if (resimDosyasi.ContentLength > 0)
+                {
+                    var resimAdi = Path.GetFileName(resimDosyasi.FileName);
+                    var resimYolu = Path.Combine(Server.MapPath("~/Content/Resimler"), resimAdi);
+                    resimDosyasi.SaveAs(resimYolu);
+                }
+            
+
+
+            return View();
+        }
+
+    }
+}
